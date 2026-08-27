f7378dd3fe76c38cab98729e19a2b87684b71298
Kaz0818 <kaz.researcher369@gmail.com>
2026-08-28 08:38:22 +0900

    feat: create custom dataset class for using kaggle dataset

---
+++ vision/segmentation/datasets/dataset.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import torch
+from PIL import Image
+from torch import Tensor
+from torch.utils.data import Dataset
+from torchvision import tv_tensors
+from torchvision.transforms import v2
+
+
+class SegmentDataset(Dataset):
+    """
+    kaggleでのDataset作成方法,最後のmaskだけmaskのunique確認して0~始まっていれば
+    -1はいらない。現状のままでok
+    images: Pathでのimages配列を想定
+    masks: Pathでのmask配列を想定
+    """
+
+    def __init__(
+        self,
+        images: list[Path],
+        masks: list[Path],
+        image_size: tuple[int, int] = (256, 256),
+        augment: bool = True,
+    ) -> None:
+        self.images = images
+        self.masks = masks
+
+        if augment:
+            self.transforms = v2.Compose(
+                [
+                    v2.Resize(image_size),
+                    v2.RandomHorizontalFlip(p=0.5),
+                ]
+            )
+        else:
+            self.transforms = v2.Compose(
+                [
+                    v2.Resize(image_size),
+                ]
+            )
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
+        image_path = self.images[index]
+        mask_path = self.masks[index]
+
+        image = Image.open(image_path).convert("RGB")
+        mask = Image.open(mask_path)
+
+        image = tv_tensors.Image(image)
+        mask = tv_tensors.Mask(mask)
+
+        image, mask = self.transforms(image, mask)
+
+        image = v2.ToDtype(
+            torch.float32,
+            scale=True,
+        )(image)
+
+        mask = mask.squeeze(0).long()
+
+        return image, mask
